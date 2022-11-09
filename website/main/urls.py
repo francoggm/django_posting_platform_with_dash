a2bc62ae9adf93c31b204eb5386921c7916ff8bb
Gabriel Franco <gabriel@asimov.academy>
2022-11-09 13:58:04 -0300

    new groups, models and user handling

--- website/main/urls.py
+++ website/main/urls.py
@@ -7,7 +7,11 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("home/", views.home, name="home"),
     path("sign-up/", views.sign_up, name="sign-up"),
+    path("create-post/", views.create_post, name="create-post"),
     path("password_change/", auth_views.PasswordChangeView.as_view(
         template_name="registration/password_change.html"
     ), name="password_change"),
+    path("login/", auth_views.LoginView.as_view(
+        template_name="registration/login.html"
+    ), name="login"),
 ]
